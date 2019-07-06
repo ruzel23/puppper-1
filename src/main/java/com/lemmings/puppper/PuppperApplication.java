5b2b932528b1b18e4d3d12aa09761dc56cda05c3
Rinat Zahoda <mrsalatikus@gmail.com>
2019-07-06 20:05:28 +0300

    Initial commit

---
+++ src/main/java/com/lemmings/puppper/PuppperApplication.java
@@ -0,0 +1,13 @@
+package com.lemmings.puppper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lemmings/puppper/PuppperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lemmings.puppper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PuppperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
