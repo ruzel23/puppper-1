6c5d80cdbb1b74a937fbd468c114e7785344838e
brddlrr <mishamavrin@gmail.com>
2019-07-14 11:55:39 +0300

    basic post entity and repo added

--- src/main/java/com/lemmings/puppper/model/Post.java
+++ src/main/java/com/lemmings/puppper/model/Post.java
@@ -1,4 +1,32 @@
 package com.lemmings.puppper.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    private Long authorId;
+    private String content;
+    private String creationDate;
+
 }
