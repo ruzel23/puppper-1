f2eabf6133295779a229b7c350a985a3f876be02
brddlrr <mishamavrin@gmail.com>
2019-07-16 16:47:42 +0300

    primitive post handling implementation

--- src/main/java/com/lemmings/puppper/controllers/PostController.java
+++ src/main/java/com/lemmings/puppper/controllers/PostController.java
@@ -1,4 +1,55 @@
 package com.lemmings.puppper.controllers;
 
+import com.lemmings.puppper.model.Post;
+import com.lemmings.puppper.services.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@Controller
+@RequestMapping("/timeline")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public String showAllPosts(Model model) {
+        List<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
+
+        return "timeline";
+    }
+
+    @GetMapping("/{id}")
+    public String showPost(@PathVariable Long id, Model model) {
+        Post post = postService.getPostById(id);
+        model.addAttribute("post", post);
+
+        return id.toString();
+    }
+
+    @PostMapping
+    public String createPost(@RequestParam("authorId") Long authorId,
+                             @RequestParam("text") String text,
+                             Model model) {
+        postService.createNewPost(authorId, text);
+
+        return "redirect:/timeline";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id,
+                         @RequestParam String text,
+                         Model model) {
+        postService.updatePost(id, text);
+
+        return "redirect:/" + id;
+    }
 }
--- src/main/java/com/lemmings/puppper/model/Post.java
+++ src/main/java/com/lemmings/puppper/model/Post.java
@@ -1,32 +1,40 @@
 package com.lemmings.puppper.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "posts")
-public class Post {
+@EqualsAndHashCode(of = {"id"})
+@Table(name = "Posts")
+public class Post implements Serializable {
 
     @Id
     @GeneratedValue
+    @Column(name = "id")
     private Long id;
 
-    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+//    @ManyToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @Column(name = "user_id")
     private Long authorId;
+    @Column(name = "content")
     private String content;
+    @Column(name = "creation_date")
     private String creationDate;
 
 }
