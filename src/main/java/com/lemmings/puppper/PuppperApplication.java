0b72c81e0ca9ddc00dc9664bd7eebb41a61efe08
ruzel23 <ruzel23@yandex.ru>
2019-07-24 23:36:58 +0300

    pull. comment module

--- src/main/java/com/lemmings/puppper/PuppperApplication.java
+++ src/main/java/com/lemmings/puppper/PuppperApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
+@EnableCaching
 public class PuppperApplication {
 
     public static void main(String[] args) {
